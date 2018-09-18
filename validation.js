2d8bbb18acb9b38f0cca70bd2fb4cbd543431c73
wakuwaku0918 <43364561+wakuwaku0918@users.noreply.github.com>
2018-09-18 17:23:27 +0900

    Update validation.js

--- validation.js
+++ validation.js
@@ -8,14 +8,15 @@ var validation = {
   },
   regExpTest: function(val, regExp)
   {
-    if(!validation.isEmpty(val) && val.match(regExp) !== null){
+    if( !validation.isEmpty(val) && val.match(regExp) !== null ){
       return true;
     }
     return false;
   },
   isEmail: function(val)
   {
-    let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,6}$/i;
+    //let regExp = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
     return validation.regExpTest(val, regExp);
   },
   isNumber: function(val)
@@ -29,23 +30,31 @@ var validation = {
     return validation.regExpTest(val, regExp);
   },
   isKatakana: function(val){
-    let regExp =/^[ァ-ヶー\s]+$/;
+    let regExp =/^[\u30a0-\u30ff]+$/;
+    return validation.regExpTest(val, regExp);
+  },
+  isHirakana: function(val){
+    let regExp =/^[\u3040-\u30ff]+$/;
+    return validation.regExpTest(val, regExp);
+  },
+  isZenkaku: function(val){
+    let regExp =/^[\u3040-\u309f]+$/;
     return validation.regExpTest(val, regExp);
   },
   chkStrMinLength: function(val, leng){
-    if(typeof val === "string" && val.length > leng){
+    if( typeof val === "string" && val.length > leng ){
       return true;
     }
     return false;
   },
   chkStrMaxLength: function(val, leng){
-    if(typeof val === "string" && val.length < leng){
+    if( typeof val === "string" && val.length < leng ){
       return true;
     }
     return false;
   },
   isStrLength: function(val, minLeng, maxLeng){
-    if(validation.chkStrMinLength(val, minLeng) && validation.chkStrMaxLength(val, maxLeng)){
+    if( validation.chkStrMinLength(val, minLeng) && validation.chkStrMaxLength(val, maxLeng) ){
       return true;
     }
     return false;
